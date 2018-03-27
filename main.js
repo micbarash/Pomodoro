c745e115ceac73c723aafad800481df1ea0ade10
Michael <micbarash@gmail.com>
2018-03-27 19:26:51 +0300

    Timer function complete

--- main.js
+++ main.js
@@ -18,10 +18,27 @@ let stopTimer = document.getElementById("stopTimer");
 let sessionLength = 25;
 let breakLength = 5;
 
+//Timer elements
+let minutes = sessionLength;
+let seconds = 60;
+let isBreak = false;
+
+runningTimer.innerHTML = sessionLength + ":" + "00"
+
+function displayTimer() {
+	if (seconds < 60) {
+		runningTimer.innerHTML = sessionLength + ":" + seconds
+	} else {
+		runningTimer.innerHTML = sessionLength + ":" + "00"
+	}
+}
+
 function incrementSession() {
 	value = Number(sessionSetting.innerHTML)
 	sessionSetting.innerHTML = value + 1
 	sessionLength = value + 1
+	minutes = sessionLength
+	displayTimer()
 };
 
 function decreaseSession() {
@@ -29,9 +46,13 @@ function decreaseSession() {
 	if (value > 1) {
 		sessionSetting.innerHTML = value - 1
 		sessionLength = value - 1
+		minutes = sessionLength
+		displayTimer()
 	} else {
 		sessionSetting.innerHTML = value
 		sessionLength = value
+		minutes = sessionLength
+		displayTimer()
 	}
 };
 
@@ -52,3 +73,33 @@ function decreaseBreak() {
 	}
 };
 
+function countdown(minutes) {
+	seconds = 60;
+    let mins = minutes
+    function tick() { 
+        let current_minutes = mins-1;
+        seconds = seconds -1;
+        runningTimer.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+        if( seconds > 0 ) {
+            setTimeout(tick, 1000);
+        } else if (runningTimer.innerHTML = "0:00" && isBreak == true) {
+            		isBreak = false;
+            		timerIndicator.innerHTML = "SESSION";
+            		countdown(sessionLength);
+        } else {
+            if(mins > 1){
+            	seconds = 60;
+                countdown(minutes -1);           
+            } else {
+            	if (seconds == 0) {
+            		isBreak = true;
+            		timerIndicator.innerHTML = "BREAK";
+            		seconds = 60;
+            		countdown(breakLength);
+            		console.log(isBreak);
+            	} 
+            }
+        }
+    }
+    tick();
+}
