470c626f76c2add4498fb3e5820ee8444260761e
Michael <micbarash@gmail.com>
2018-03-30 01:33:24 +0300

    All main timer functions completed

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ let decSession = document.getElementById("sessionDown");
 let incBreak = document.getElementById("breakUp");
 let decBreak = document.getElementById("breakDown");
 let runTimer = document.getElementById("runTimer");
-let refreshTimer = document.getElementById("refreshTimer");
-let pauseTimer = document.getElementById("pauseTimer");
-let stopTimer = document.getElementById("stopTimer");
+let refreshTimerButton = document.getElementById("refreshTimer");
+let pauseTimerButton = document.getElementById("pauseTimer");
+let stopTimerButton = document.getElementById("stopTimer");
 
 //Counters
 let sessionLength = 25;
@@ -22,6 +22,8 @@ let breakLength = 5;
 let minutes = sessionLength;
 let seconds = 60;
 let isBreak = false;
+let stop = false;
+let run;
 
 runningTimer.innerHTML = sessionLength + ":" + "00"
 
@@ -34,72 +36,118 @@ function displayTimer() {
 }
 
 function incrementSession() {
-	value = Number(sessionSetting.innerHTML)
-	sessionSetting.innerHTML = value + 1
-	sessionLength = value + 1
-	minutes = sessionLength
-	displayTimer()
+	if (seconds == 0) {
+		value = Number(sessionSetting.innerHTML)
+		sessionSetting.innerHTML = value + 1
+		sessionLength = value + 1
+		minutes = sessionLength
+		displayTimer()
+	}
 };
 
 function decreaseSession() {
-	value = Number(sessionSetting.innerHTML)
-	if (value > 1) {
-		sessionSetting.innerHTML = value - 1
-		sessionLength = value - 1
-		minutes = sessionLength
-		displayTimer()
-	} else {
-		sessionSetting.innerHTML = value
-		sessionLength = value
-		minutes = sessionLength
-		displayTimer()
+	if (seconds == 0) {
+		value = Number(sessionSetting.innerHTML)
+		if (value > 1) {
+			sessionSetting.innerHTML = value - 1
+			sessionLength = value - 1
+			minutes = sessionLength
+			displayTimer()
+		} else {
+			sessionSetting.innerHTML = value
+			sessionLength = value
+			minutes = sessionLength
+			displayTimer()
+		}
 	}
 };
 
 function incrementBreak() {
-	value = Number(breakSetting.innerHTML)
-	breakSetting.innerHTML = value + 1
-	breakLength = value + 1
+	if (seconds == 0) {
+		value = Number(breakSetting.innerHTML)
+		breakSetting.innerHTML = value + 1
+		breakLength = value + 1		
+	}
 };
 
 function decreaseBreak() {
-	value = Number(breakSetting.innerHTML)
-	if (value > 1) {
-		breakSetting.innerHTML = value - 1
-		breakLength = value - 1
-	} else {
-		breakSetting.innerHTML = value
-		breakLength = value
+	if (seconds == 0) {
+		value = Number(breakSetting.innerHTML)
+		if (value > 1) {
+			breakSetting.innerHTML = value - 1
+			breakLength = value - 1
+		} else {
+			breakSetting.innerHTML = value
+			breakLength = value
+		}
 	}
 };
 
 function countdown(minutes) {
-	seconds = 60;
     let mins = minutes
+    stop = false;
     function tick() { 
         let current_minutes = mins-1;
-        seconds = seconds -1;
+        seconds--;
         runningTimer.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            setTimeout(tick, 1000);
+        if (current_minutes < 1 && seconds < 10 && seconds > 0) {
+        	runningTimer.style.color = "Red";
+        } else {
+        	runningTimer.style.color = "White";
+        }
+        if ( seconds > 0 ) {
+        	if (stop === false) {
+           	  run = setTimeout(tick, 1000)
+        	}
         } else if (runningTimer.innerHTML = "0:00" && isBreak == true) {
             		isBreak = false;
             		timerIndicator.innerHTML = "SESSION";
+            		timerIndicator.style.color = "White";
             		countdown(sessionLength);
         } else {
-            if(mins > 1){
+            if (mins > 1) {
             	seconds = 60;
-                countdown(minutes -1);           
+                countdown(minutes -1);        
             } else {
             	if (seconds == 0) {
             		isBreak = true;
             		timerIndicator.innerHTML = "BREAK";
+            		timerIndicator.style.color = "Green";
             		seconds = 60;
             		countdown(breakLength);
-            		console.log(isBreak);
-            	} 
+            	}
             }
         }
     }
-    tick();
+    if (stop === false) {
+    	tick();
+	}
+}
+
+function pause() {
+	stop = true;
+	clearTimeout(run);
+}
+
+function stopTimer() {
+		stop = true;
+		clearTimeout(run);
+		seconds = 60;
+		minutes = sessionLength;
+		runningTimer.innerHTML = minutes.toString() + ":" + "00"
+		timerIndicator.innerHTML = "SESSION";
+}
+
+function refreshTimer() {
+		stop = true;
+		clearTimeout(run);
+		seconds = 60;
+		sessionLength = 25;
+		breakLength = 5;
+		sessionSetting.innerHTML = sessionLength.toString();
+		breakSetting.innerHTML = breakLength.toString();
+		minutes = sessionLength;
+		runningTimer.innerHTML = minutes.toString() + ":" + "00"
+		timerIndicator.innerHTML = "SESSION";
+	
 }
